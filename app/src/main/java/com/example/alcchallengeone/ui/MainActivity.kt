df1897f97447ef8b9f5d734072c8f245b0169dee
festus <kiamfestoh@gmail.com>
2019-07-17 13:01:20 +0300

    initial challenge

---
+++ app/src/main/java/com/example/alcchallengeone/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.alcchallengeone.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.alcchallengeone.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
